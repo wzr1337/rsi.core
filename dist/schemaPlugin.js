e1b021c172c7be41bc27093768075b9958473332
Benjamin Dobler <benz@richapps.de>
2018-02-09 23:08:30 +0100

    feat(schemaPlugin): Changed SchemaPlugin Resource methods to return promises

--- dist/schemaPlugin.js
+++ dist/schemaPlugin.js
@@ -9,6 +9,41 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = y[op[0] & 2 ? "return" : op[0] ? "throw" : "next"]) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [0, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var fs = require("fs");
 var path = require("path");
@@ -75,26 +110,35 @@ var SchemaPlugin = /** @class */ (function (_super) {
                     this_1.updateUris(data);
                     var resource = new SchemaResource(this_1, resourceDef, data, content.resources[resourceDef]);
                     this_1.resourceMap[resourceDef] = resource;
-                    resource.change.subscribe(function () {
-                        var rawData = resource.getResource().data.map(function (value) {
-                            return value.getValue().data;
+                    resource.change.subscribe(function () { return __awaiter(_this, void 0, void 0, function () {
+                        var collectionResponse, rawData, srcPath;
+                        return __generator(this, function (_a) {
+                            switch (_a.label) {
+                                case 0: return [4 /*yield*/, resource.getResource()];
+                                case 1:
+                                    collectionResponse = _a.sent();
+                                    rawData = collectionResponse.data.map(function (value) {
+                                        return value.getValue().data;
+                                    });
+                                    this.data[resource.name] = rawData;
+                                    srcPath = path.join(this.pluginDir, 'data.json');
+                                    srcPath = srcPath.replace('bin', 'src');
+                                    try {
+                                        fs.writeFileSync(srcPath, JSON.stringify(this.data, null, 4), { encoding: 'utf-8' }); // persist data also to src path otherwise it will be lost with each rebuild
+                                    }
+                                    catch (d) {
+                                        console.log('Error writing data file src ', d);
+                                    }
+                                    try {
+                                        fs.writeFileSync(path.join(__dirname, this.name, 'data.json'), JSON.stringify(this.data, null, 4), { encoding: 'utf-8' });
+                                    }
+                                    catch (e) {
+                                        //console.log("Error writing data file bin");
+                                    }
+                                    return [2 /*return*/];
+                            }
                         });
-                        _this.data[resource.name] = rawData;
-                        var srcPath = path.join(_this.pluginDir, 'data.json');
-                        srcPath = srcPath.replace('bin', 'src');
-                        try {
-                            fs.writeFileSync(srcPath, JSON.stringify(_this.data, null, 4), { encoding: 'utf-8' }); // persist data also to src path otherwise it will be lost with each rebuild
-                        }
-                        catch (d) {
-                            console.log('Error writing data file src ', d);
-                        }
-                        try {
-                            fs.writeFileSync(path.join(__dirname, _this.name, 'data.json'), JSON.stringify(_this.data, null, 4), { encoding: 'utf-8' });
-                        }
-                        catch (e) {
-                            //console.log("Error writing data file bin");
-                        }
-                    });
+                    }); });
                     this_1.resources.push(resource);
                 };
                 var this_1 = this;
@@ -176,86 +220,114 @@ var SchemaResource = /** @class */ (function () {
         configurable: true
     });
     SchemaResource.prototype.getElement = function (elementId) {
-        return {
-            status: 'ok',
-            data: this._elements.find(function (element) {
-                return element.getValue().data.id === elementId;
-            })
-        };
+        return __awaiter(this, void 0, void 0, function () {
+            return __generator(this, function (_a) {
+                return [2 /*return*/, {
+                        status: 'ok',
+                        data: this._elements.find(function (element) {
+                            return element.getValue().data.id === elementId;
+                        })
+                    }];
+            });
+        });
     };
     ;
     SchemaResource.prototype.getResource = function (offset, limit) {
-        var resp;
-        var o = 0;
-        var l = this._elements.length;
-        if (offset && typeof offset === 'number') {
-            o = offset;
-        }
-        else if (offset && typeof offset === 'string') {
-            var el = this._elements.find(function (x) { return x.getValue().data.id === offset; });
-            if (el) {
-                o = this._elements.indexOf(el);
-            }
-        }
-        if (limit && typeof limit === 'number') {
-            l = o + limit;
-        }
-        else if (limit && typeof limit === 'string') {
-            var el = this._elements.find(function (x) { return x.getValue().data.id === limit; });
-            if (el) {
-                l = this._elements.indexOf(el) + 1;
-            }
-        }
-        resp = this._elements.slice(o, l);
-        return { status: 'ok', data: resp };
+        return __awaiter(this, void 0, void 0, function () {
+            var resp, o, l, el, el;
+            return __generator(this, function (_a) {
+                o = 0;
+                l = this._elements.length;
+                if (offset && typeof offset === 'number') {
+                    o = offset;
+                }
+                else if (offset && typeof offset === 'string') {
+                    el = this._elements.find(function (x) { return x.getValue().data.id === offset; });
+                    if (el) {
+                        o = this._elements.indexOf(el);
+                    }
+                }
+                if (limit && typeof limit === 'number') {
+                    l = o + limit;
+                }
+                else if (limit && typeof limit === 'string') {
+                    el = this._elements.find(function (x) { return x.getValue().data.id === limit; });
+                    if (el) {
+                        l = this._elements.indexOf(el) + 1;
+                    }
+                }
+                resp = this._elements.slice(o, l);
+                return [2 /*return*/, { status: 'ok', data: resp }];
+            });
+        });
     };
     ;
     SchemaResource.prototype.getResourceSpec = function () {
         return this.spec;
     };
     SchemaResource.prototype.updateElement = function (elementId, difference) {
-        var element = this.getElement(elementId).data;
-        var collection = element.getValue();
-        var newData = Object.assign({}, collection.data, difference);
-        element.next({
-            lastUpdate: Date.now(),
-            propertiesChanged: [],
-            data: newData
+        return __awaiter(this, void 0, void 0, function () {
+            var elementResponse, element, collection, newData;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, this.getElement(elementId)];
+                    case 1:
+                        elementResponse = _a.sent();
+                        element = elementResponse.data;
+                        collection = element.getValue();
+                        newData = Object.assign({}, collection.data, difference);
+                        element.next({
+                            lastUpdate: Date.now(),
+                            propertiesChanged: [],
+                            data: newData
+                        });
+                        this._change.next({ lastUpdate: Date.now(), action: 'update', oldValue: collection.data, newValue: newData });
+                        return [2 /*return*/, { status: 'ok' }];
+                }
+            });
         });
-        this._change.next({ lastUpdate: Date.now(), action: 'update', oldValue: collection.data, newValue: newData });
-        return { status: 'ok' };
     };
     SchemaResource.prototype.createElement = function (state) {
-        if (!state.name)
-            return {
-                status: 'error',
-                error: new Error('providing a name is mandatory'),
-                code: rsiPlugin_1.StatusCode.INTERNAL_SERVER_ERROR
-            };
-        if (!state.id) {
-            state.id = uuid.v1();
-        }
-        state.uri = '/' + this.service.name.toLowerCase() + '/' + this.name.toLowerCase() + '/' + state.id;
-        var newElement = new rxjs_1.BehaviorSubject({
-            lastUpdate: Date.now(),
-            propertiesChanged: [],
-            data: state
+        return __awaiter(this, void 0, void 0, function () {
+            var newElement;
+            return __generator(this, function (_a) {
+                if (!state.name)
+                    return [2 /*return*/, {
+                            status: 'error',
+                            error: new Error('providing a name is mandatory'),
+                            code: rsiPlugin_1.StatusCode.INTERNAL_SERVER_ERROR
+                        }];
+                if (!state.id) {
+                    state.id = uuid.v1();
+                }
+                state.uri = '/' + this.service.name.toLowerCase() + '/' + this.name.toLowerCase() + '/' + state.id;
+                newElement = new rxjs_1.BehaviorSubject({
+                    lastUpdate: Date.now(),
+                    propertiesChanged: [],
+                    data: state
+                });
+                this._elements.push(newElement);
+                this._change.next({ lastUpdate: Date.now(), action: 'add' });
+                return [2 /*return*/, { status: 'ok', data: newElement }];
+            });
         });
-        this._elements.push(newElement);
-        this._change.next({ lastUpdate: Date.now(), action: 'add' });
-        return { status: 'ok', data: newElement };
     };
     ;
     SchemaResource.prototype.deleteElement = function (elementId) {
-        var idx = this._elements.findIndex(function (element, index) {
-            return element.getValue().data.id === elementId;
+        return __awaiter(this, void 0, void 0, function () {
+            var idx;
+            return __generator(this, function (_a) {
+                idx = this._elements.findIndex(function (element, index) {
+                    return element.getValue().data.id === elementId;
+                });
+                if (-1 !== idx) {
+                    this._elements.splice(idx, 1); //remove one item from the collections array
+                    this._change.next({ lastUpdate: Date.now(), action: 'remove' });
+                    return [2 /*return*/, { status: 'ok' }];
+                }
+                return [2 /*return*/, { status: 'error', code: 404, message: 'Element can not be found' }];
+            });
         });
-        if (-1 !== idx) {
-            this._elements.splice(idx, 1); //remove one item from the collections array
-            this._change.next({ lastUpdate: Date.now(), action: 'remove' });
-            return { status: 'ok' };
-        }
-        return { status: 'error', code: 404, message: 'Element can not be found' };
     };
     return SchemaResource;
 }());
