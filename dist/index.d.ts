21b7dc17df8bacecb924840e4b36d147e85b3601
Denny Trebbin <denny.trebbin@gmail.com>
2018-06-08 00:02:11 +0200

    chore(cleanup): cleanup before release

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { Service, Resource, IElement, IResourceUpdate, StatusCode } from "./rsiPlugin";
-export { XObject } from "./xobject";
-export { ElementResponse, CollectionResponse, Response } from "./response";
-export { SchemaPlugin } from "./schemaPlugin";
-export { SchemaResource } from "./schemaResource";
-export { RsiLogger, IRsiLoggerInstance } from "./rsiLogger";
