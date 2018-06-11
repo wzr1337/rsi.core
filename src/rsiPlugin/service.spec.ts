7526f42c8a53253a9041c985a416adc187d9ee41
Dr.Patrick Bartsch <bartsch98@gmail.com>
2018-06-11 18:41:40 +0200

    chrore(tests): add missing semicolons

--- src/rsiPlugin/service.spec.ts
+++ src/rsiPlugin/service.spec.ts
@@ -2,13 +2,15 @@ import "jasmine";
 import { Service } from "./service";
 
 describe("Service", () => {
+
     it("should be instantiated", () => {
         const service = Service.getInstance();
-        expect(service).toBeDefined()
+        expect(service).toBeDefined();
     });
+
     it("should have an id", () => {
         const service = Service.getInstance();
         expect(service.id).toBeDefined();
         expect(service.id).toBe("no id set");
-    })
+    });
 });
